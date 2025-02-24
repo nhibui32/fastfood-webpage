336a021a1348b2df0cb41d29ea0fe7e2e7eacfb7
n2ai <longhai2511@gmail.com>
2025-02-23 20:01:39 -0600

    UPdate on APIs

--- app-BE/controllers/controllers.js
+++ app-BE/controllers/controllers.js
@@ -6,13 +6,23 @@ import { createJWT } from "../middlewares/jwt.js";
 export const loginController = async (req, res)=>{
     const {email, password} = req.body;
     try{
-        const {data, error} = await supabase.from("Users").select("email, hashed_password, id").eq("email", email);
+        const {data_users_table, users_table_error} = await supabase.from("Users")
+        .select("email, id, credentials[i")
+        .eq("email", email);
         if (error){
-            return res.status(500).json({error:error.message});
+            return res.status(500).json({error:users_table_error.message});
         };
 
-        if (data.length === 0){
+        if (data_users_table.length === 0){
             return res.status(400).json({error: "Wrong Password or Account Does not Exists!"});
+        };
+        
+        const {data_credentials_table, credentials_table_error} = await supabase.from("Credentials")
+        .select("hashed_password")
+        .eq("users_id", data_credentials_table.id)
+
+        if (error){
+            return res.status(500).json({error:credentials_table_error});
         };
 
         const hashed_password = data.hashed_password;
@@ -23,14 +33,14 @@ export const loginController = async (req, res)=>{
         //CREATE JWT Payload
         const jwtPayload = {
             email: email,
-            id:data.id,
+            id:data_users_table.id,
             expires_in : process.env.JWT_EXPIRES_IN
         };
 
         const token = createJWT(jwtPayload);
 
         const responseData = {
-            id: data.id,
+            id: data_users_table.id,
             accessToken: token
         };
         
@@ -55,7 +65,7 @@ export const signupController = async (req,res)=>{
 
         const salt = 10;
         bcrypt.hash(password, salt, (error, hash)=>{
-            
+
         })
 
     }catch(error){
