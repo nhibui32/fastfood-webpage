a52c0939ae96b998559a2afb20e1d96a56cddfb1
n2ai <longhai2511@gmail.com>
2025-02-25 23:06:24 -0600

    Finish SignUp Features

--- app-BE/controllers/controllers.js
+++ app-BE/controllers/controllers.js
@@ -6,8 +6,8 @@ import { createJWT } from "../middlewares/jwt.js";
 export const loginController = async (req, res)=>{
     const {email, password} = req.body;
     try{
-        const {data_users_table, users_table_error} = await supabase.from("Users")
-        .select("email, id, credentials[i")
+        const {data:data_users_table, error:users_table_error} = await supabase.from("Users")
+        .select("email, id, credentials_id")
         .eq("email", email);
         if (error){
             return res.status(500).json({error:users_table_error.message});
@@ -17,15 +17,15 @@ export const loginController = async (req, res)=>{
             return res.status(400).json({error: "Wrong Password or Account Does not Exists!"});
         };
         
-        const {data_credentials_table, credentials_table_error} = await supabase.from("Credentials")
+        const {data:data_credentials_table, error:credentials_table_error} = await supabase.from("Credentials")
         .select("hashed_password")
-        .eq("users_id", data_credentials_table.id)
+        .eq("users_id", data_users_table[0]?.id)
 
         if (error){
             return res.status(500).json({error:credentials_table_error});
         };
 
-        const hashed_password = data.hashed_password;
+        const hashed_password = data_credentials_table.hashed_password;
         if(!bcrypt(password, hashed_password)){
             return res.status(400).json({error: "Wrong Password or Account Does not Exists!"})
         };
@@ -62,11 +62,38 @@ export const signupController = async (req,res)=>{
         if(data.length !== 0){
             return res.status(400).json({error:"The account is already exists"});
         }
-
+        
         const salt = 10;
-        bcrypt.hash(password, salt, (error, hash)=>{
+        bcrypt.hash(password, salt, async (error, hash)=>{
+            if(error){
+                return res.status(500).json({error:error});
+            }
+
+            const{data:data_credentials_table, error:error_credentials_table} = await supabase.from("Credentials").insert([
+            {"hashed_password": hash}
+            ])
+            .select("id");
+
+            if(error_credentials_table){
+                return res.status(500).json({error:error_credentials_table});
+            }
+
+            const{data:data_users_table, error:error_users_table} = await supabase.from("Users").insert([{
+                "name":name, "email":email, "phone_number":phoneNumber,"credentials_id":data_credentials_table[0]?.id
+            }])
+            .select("id");
+
+            if(error_users_table){
+                return res.status(500).json({error:error_users_table});
+            }
+
+            await supabase.from("Credentials").update({
+                "users_id":data_users_table[0]?.id
+            }).eq("id",data_credentials_table[0]?.id)
 
         })
+
+        return res.status(200).json({message:"Account Created Successfully!"})
 
     }catch(error){
         return res.status(500).json({error:error})
--- app-BE/routes/credentials.js
+++ app-BE/routes/credentials.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 import supabase from "../config/database.js";
-import { loginController } from "../controllers/controllers.js";
-router.get("/login", loginController);
-
+import { loginController, signupController } from "../controllers/controllers.js";
+router.get("/credentials", loginController);
+router.post("/credentials",signupController);
 export default router;
